0eb698aacfa338565c2891067d256c11033a08c7
Malcolm Edwards <nazxuul@gmail.com>
2018-10-02 20:05:33 +1000

    project created

---
+++ fortune-rpg/Services/DataService.swift
@@ -0,0 +1,12 @@
+//
+//  DataService.swift
+//  fortune-rpg
+//
+//  Created by Malcolm Edwards on 2/10/18.
+//  Copyright © 2018 Xenophile Games. All rights reserved.
+//
+
+import Foundation
+
+
+
