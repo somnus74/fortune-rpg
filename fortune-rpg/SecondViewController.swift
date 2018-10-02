972826ecf867f81b1df0a240aa7cdf7283aaaf8f
Malcolm Edwards <nazxuul@gmail.com>
2018-10-02 19:46:00 +1000

    Initial Commit

---
+++ fortune-rpg/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  fortune-rpg
+//
+//  Created by Malcolm Edwards on 2/10/18.
+//  Copyright © 2018 Xenophile Games. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
